504ade7f91c10cb92b53520fe2b6a3abba6ef71c
jim <test@youremail.com>
2015-12-06 21:49:08 -0500

    update scripts.js 9:48 p.m. files

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-function setup() {}
+function setup() {
+	// Create the canvas
+  createCanvas(720, 400);
+  background(200);
+  
+  // Set colors
+  fill(204, 101, 192, 127);
+  stroke(127, 63, 120);
+	
+}
 
 function draw() {
   ellipse(50, 50, 80, 80);
