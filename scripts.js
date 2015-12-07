59e55f629020c13c6a37d79e3a77d72c4725cbf7
jim <test@youremail.com>
2015-12-06 21:54:45 -0500

    update scripts.js 9:54 p.m. files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// This is a comments.
+var  meeting_date = [  "no Month", "January 20", "February 17", "March 17", "April 21", "May 19", "June 16", "July 21", "August 18", "September 15", "October 20", "November 17", "No Meeting(Christmas Party)","January 19"];
 function setup() {
 	// Create the canvas
   createCanvas(720, 400);
