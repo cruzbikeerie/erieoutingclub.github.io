fbcc783c9e25c3fabdec376ead8bcbcc492ab13e
jim <test@youremail.com>
2015-12-08 11:03:53 -0500

    update scripts.js12/08/2015 10:47 a.m.

--- scripts.js
+++ scripts.js
@@ -2,18 +2,11 @@
 var  meeting_date = [  "no Month", "January 20", "February 17", "March 17", "April 21", "May 19", "June 16", "July 21", "August 18", "September 15", "October 20", "November 17", "No Meeting(Christmas Party)","January 19"];
 var meeting_location = ["no Month", "at the Sight Center", "at the Sight Center", "at the Sight Center", "at the Sight Center", "at the Sight Center", "at the  Rotary Pavilion", "at the  Rotary Pavilion", "at the  Rotary Pavilion", "at the Sight Center", "at the Sight Center", "at the Sight Center", " at the Beechwood Restaurant"]
 function setup() {
-	// Create the canvas
-	background(#33CC00);
-	createCanvas(720, 400);
-	print("Hello")
-  //noStroke();
-   ellipse(50, 50, 80, 80);
+	print("i am working")
   
  
   
-  // Set colors
-  fill(204, 101, 192, 127);
-  
+  /
 	
 }
 
