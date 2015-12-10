bcf7fef0641635a43b6f6cc1bf77653626115793
erieoutingclub <erieoutingclub@gmail.com>
2015-12-10 12:03:16 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// This is a comments.
+// updated Demcember
 var  meeting_date = [  "no Month", "January 20", "February 17", "March 17", "April 21", "May 19", "June 16", "July 21", "August 18", "September 15", "October 20", "November 17", "No Meeting(Christmas Party)","January 19"];
 var meeting_location = ["no Month", "at the Sight Center", "at the Sight Center", "at the Sight Center", "at the Sight Center", "at the Sight Center", "at the  Rotary Pavilion", "at the  Rotary Pavilion", "at the  Rotary Pavilion", "at the Sight Center", "at the Sight Center", "at the Sight Center", " at the Beechwood Restaurant"]
 function setup() {
