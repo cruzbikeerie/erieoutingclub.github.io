1f8b91e0f26b9f5936f626a341e63617de009071
erieoutingclub <erieoutingclub@gmail.com>
2015-12-10 21:27:16 -0500

    Update p5_javascript.js

--- p5_javascript.js
+++ p5_javascript.js
@@ -1,7 +1,15 @@
+
+var person = {
+    firstName:"John",
+    lastName:"Doe",
+    age:50,
+    eyeColor:"blue"
+};
 function setup() {
 
 }
 
 function draw() {
   ellipse(50, 50, 80, 80);
+  print(person.firstName);
 }
