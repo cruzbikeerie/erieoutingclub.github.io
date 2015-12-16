02ed7337778f84e781efe96808e7b3860615851f
erieoutingclub <erieoutingclub@gmail.com>
2015-12-15 20:26:56 -0500

    Update p5_javascript.js

--- p5_javascript.js
+++ p5_javascript.js
@@ -12,5 +12,9 @@ createCanvas(900, 400);
 
 function draw() {
     fill(255,0,0);
-  rect(10,19,hieght - 10, width - 10);
+  rect(10,19,height - 10, width - 10);
+   rect(0,height * .25 ,height * .25, width * .75);
+    rect(0,(height * .25) * 2,height * .25, width *.75);
+     rect(0,(height * .25) * 3,height * .25, width  * .75);
+      rect(0,(height * .25) * 4,height * .25, width * .75);
 }
