bef0f4ac406818d6cc715ab0c0f50cf7c144044e
erieoutingclub <erieoutingclub@gmail.com>
2016-04-29 15:40:35 -0400

    Update 123forms.js

--- static/js/123forms.js
+++ static/js/123forms.js
@@ -1,9 +1,1 @@
- var servicedomain = "www.123contactform.com";
-          var frmRef = '';
-          try {
-            frmRef = window.top.location.href;
-          } catch (err) {};
-          var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
-          document.write(unescape("%3Cscript src='" + cfJsHost + servicedomain + "/includes/easyXDM.min.js' type='text/javascript'%3E%3C/script%3E"));
-          frmRef = encodeURIComponent(frmRef).replace('%26', '[%ANDCHAR%]');
-          document.write(unescape("%3Cscript src='" + cfJsHost + servicedomain + "/jsform-1831479.js?ref=" + frmRef + "' type='text/javascript'%3E%3C/script%3E"));
+var servicedomain="www.123contactform.com";var frmRef='';try{frmRef=window.top.location.href;}catch(err){};var cfJsHost=(("https:"==document.location.protocol)?"https://":"http://");document.write(unescape("%3Cscript src='"+cfJsHost+servicedomain+"/includes/easyXDM.min.js' type='text/javascript'%3E%3C/script%3E"));frmRef=encodeURIComponent(frmRef).replace('%26','[%ANDCHAR%]');document.write(unescape("%3Cscript src='"+cfJsHost+servicedomain+"/jsform-1831479.js?ref="+frmRef+"' type='text/javascript'%3E%3C/script%3E"));
